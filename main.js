3cb922ab36a0a58bdd454423c7c2aa46ab3780c5
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-08 07:50:24 -0800

    Q 18 of Ts/js & node js of assignment.
    Q 18 of Ts/js & node js of assignment.

---
+++ main.js
@@ -0,0 +1,18 @@
+var places = ["Cape town", "Dehli", "Faisalabad", "Islamabad", "Muree"];
+//for original order 
+console.log(places);
+//for .sort method 
+console.log(places.sort());
+//for alphabetical order
+console.log(places);
+//for .reverse method
+console.log(places.sort());
+//for original order
+console.log(places);
+//for original order 
+var sortedplaces = ["Cape town", "Dehli", "Faisalabad", "Islamabad", "Muree"];
+//for .sort method
+sortedplaces.sort();
+console.log(sortedplaces);
+sortedplaces.reverse();
+console.log(sortedplaces);
---
+++ main.ts
@@ -0,0 +1,25 @@
+let places : string[] = ["Cape town","Dehli","Faisalabad","Islamabad","Muree"];
+//for original order 
+console.log(places);
+//for .sort method 
+console.log(places.sort());
+//for alphabetical order
+console.log(places);
+//for .reverse method
+console.log(places.sort());
+//for original order
+console.log(places);
+//for original order 
+let sortedplaces: string[] = ["Cape town","Dehli","Faisalabad","Islamabad","Muree"];
+//for .sort method
+sortedplaces.sort();
+console.log(sortedplaces);
+sortedplaces.reverse();
+console.log(sortedplaces)
+
+
+
+
+
+
+
